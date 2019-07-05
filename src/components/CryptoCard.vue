35777ab15f16d93b0f72104e18f42c16397a41b4
Andras Sebo <anse01@betssongroup.com>
2019-07-05 15:27:37 +0200

    first try

---
+++ src/components/CryptoCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-card>
+      <b-card-img :src="require('../assets/bitcoin_logo.png')" height="80" width="80"></b-card-img>
+      <b-card-text>{{ title }}</b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Crypto",
+  props: {
+    title: String
+  }
+};
+</script>
